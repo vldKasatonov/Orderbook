10f7aa36b5af68e86791f8b5a1f72500fbce10ce
Vladyslav Kasatonov <vld.kasatonov@gmail.com>
2025-07-03 19:07:42 +0300

    describe the main functionality of the orderbook

--- src/Orderbook.h
+++ src/Orderbook.h
@@ -1,12 +1,28 @@
 #ifndef ORDERBOOK_H
 #define ORDERBOOK_H
 
-
+#include "Order.h"
+#include <queue>
+#include <vector>
 
 class Orderbook {
+private:
+	class BuyOrdersComparator {
+		public:
+			bool operator()(const Order &a, const Order &b);
+	};
+	class SellOrdersComparator {
+		public:
+			bool operator()(const Order &a, const Order &b);
+	};
 
+	std::priority_queue<Order, std::vector<Order>, BuyOrdersComparator> buy_orders;
+	std::priority_queue<Order, std::vector<Order>, SellOrdersComparator> sell_orders;
+	std::vector<Order> order_history;
+
+public:
+	void match_orders();
+	void add_order(const Order &order);
 };
-
-
 
 #endif
