406be7d74303253e5035882f57d818ced5bb8ff8
Vladyslav Kasatonov <vld.kasatonov@gmail.com>
2025-07-03 23:44:52 +0300

    realise main and read_order functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "Orderbook.h"
+
+Order read_order() {
+	int user_id, amount, price;
+	bool side;
+	std::cin >> user_id >> amount >> price >> side;
+	return {user_id, amount, price, side};
+}
 
 int main() {
-
+	Orderbook orderbook;
+	std::string choice;
+	try {
+		while (true) {
+			std::cout << "Enter order[user_id, amount, price, side(0 for BUY, 1 for SELL)]:\n";
+			Order order = read_order();
+			orderbook.add_order(order);
+			std::cout << "Enter \"exit\" if you want to quit:\n";
+			std::cin >> choice;
+			if (choice == "exit") {
+				break;
+			}
+		}
+	} catch (std::invalid_argument &e) {
+		std::cout << e.what() << "\n";
+	}
 	return 0;
 }
